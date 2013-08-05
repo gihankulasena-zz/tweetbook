5045a2b6e7ff171e968115cedbe5b808b48dece6
Gihan <gihan798114@gmail.com>
2013-08-05 11:42:31 +0530

    should find a way to show the full name, mostly completed now

---
+++ app/helpers/statuses_helper.rb
@@ -0,0 +1,5 @@
+module StatusesHelper
+def full_name 
+	first_name + " " + last_name 
+end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tweetbook::Application.config.secret_key_base = '258aa0e63d49eb35ba86eb0eb16d4722fb99213b81ad999ac0da188a7641483f0b2bfe6a5d5f8a50ff6439c429a26a2baaae901115c08e705ef749b9bf112805'
